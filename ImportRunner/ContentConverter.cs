6282c89e4c887352b69e5c3d0d6fa5d7e660c4c2
alejandro <alejandro@MASCARADEWORK>
2016-08-11 23:26:50 -0400

    Some test done

--- ImportRunner/ContentConverter.cs
+++ ImportRunner/ContentConverter.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using System.Web;
 using Common.Extensions;
-using Fizzler.Systems.HtmlAgilityPack;
 using HtmlAgilityPack;
 
 namespace ImportRunner
@@ -37,25 +32,40 @@ namespace ImportRunner
         private void TreeWalker(HtmlNode node)
         {
             //string style = node.GetAttributeValue("style", string.Empty);
-            
 
-            if (node.Name == "div" && node.GetAttributeValue("class", "") == "gfyitem")
+
+            //if (node.Name == "div" && node.GetAttributeValue("class", "") == "gfyitem")
+            //{
+            //    //source src=""https://zippy.gfycat.com/ShrillWelcomeGeese.mp4
+            //    //[MEDIA=gfycat]height=320;id=CandidImmaterialDromedary;width=640[/MEDIA]
+            //    HtmlNode separatedNode = HtmlNode.CreateNode(node.OuterHtml).SelectSingleNode("//source");
+            //    string src = separatedNode.GetAttributeValue("src", string.Empty);
+            //    Regex reg = new Regex(@".*gfycat.com/");
+            //    string toRemove = reg.Match(src).Value;
+            //    src = src.Replace(toRemove, string.Empty).Replace(".mp4", string.Empty).Replace(".webm", string.Empty).Replace(".ogg", string.Empty);
+            //    _data.Append($"[MEDIA=gfycat]height=320;id={src};width=640[/MEDIA]");
+            //    return;
+            //}
+            //if (node.Name == "object")
+            //{
+            //    return;
+            //}
+            if (node.Name == "a")
             {
-                //source src=""https://zippy.gfycat.com/ShrillWelcomeGeese.mp4
-                //[MEDIA=gfycat]height=320;id=CandidImmaterialDromedary;width=640[/MEDIA]
-                HtmlNode separatedNode = HtmlNode.CreateNode(node.OuterHtml).SelectSingleNode("//source");
-                string src = separatedNode.GetAttributeValue("src", string.Empty);
-                Regex reg = new Regex(@".*gfycat.com/");
-                string toRemove = reg.Match(src).Value;
-                src = src.Replace(toRemove, string.Empty).Replace(".mp4", string.Empty).Replace(".webm", string.Empty).Replace(".ogg", string.Empty);
-                _data.Append($"[MEDIA=gfycat]height=320;id={src};width=640[/MEDIA]");
+                string src = node.GetAttributeValue("href", null);
+                if (src == null)
+                {
+                    throw new Exception("missing Source");
+                }
+                _data.Append($"[url={src}]");
+                foreach (HtmlNode childNode in node.ChildNodes)
+                {
+                    TreeWalker(childNode);
+                }
+                _data.Append("[/url]");
                 return;
             }
-            if (node.Name == "object")
-            {
-                return;
-            }
-                if (node.Name == "b")
+            if (node.Name == "b")
             {
                 _data.Append("[b]");
                 foreach (HtmlNode childNode in node.ChildNodes)
@@ -87,8 +97,6 @@ namespace ImportRunner
             }
             if (node.Name == "div" && node.ChildNodes.Count > 1 && node.ChildNodes[1].InnerText.TrimSafely().Equals("Spoiler:&nbsp;"))
             {
-                // spoiler
-                int g = 9;
                 // find the node and tree walk out of here
                 HtmlNode separatedNode = HtmlNode.CreateNode(node.OuterHtml);
                 HtmlNode messageNode = separatedNode.SelectSingleNode("/div/div[2]/div");
@@ -102,7 +110,7 @@ namespace ImportRunner
                 return;
             }
 
-            if (node.Name == "div" && node.GetAttributeValue("class", "") == "bbcode_container")
+            if (node.Name == "div" && node.GetAttributeValue("class", null) == "bbcode_container")
             {
                 // special case. here we have to dig one level to get the post by. and also skip those from the output
                 // two kinds of quote, by users and outside people
@@ -111,12 +119,12 @@ namespace ImportRunner
                 HtmlNode testForInternalQuote = separatedNode.SelectSingleNode("/div/div/div/div[2]");
                 if (testForInternalQuote != null && testForInternalQuote.HasChildNodes)
                 {
-                    string userName = separatedNode.SelectSingleNode("/div/div/div/div/strong").InnerText;
+                    string userName = HttpUtility.HtmlDecode(separatedNode.SelectSingleNode("/div/div/div/div/strong").InnerText.RemoveEndOfLineCharacter().ReplaceTabsForSingleWhiteSpace().TrimSafely());
 
-                    string postIdRef = node.SelectSingleNode("/div/div/div/div/a")?.GetAttributeValue("href", string.Empty);
-                    if (postIdRef.IsNullOrWhiteSpace())
+                    string postIdRef = node.SelectSingleNode("/div/div/div/div/a")?.GetAttributeValue("href", null);
+                    if (postIdRef == null)
                     {
-                        _data.AppendLine().Append($"[quote='{userName}']");
+                        _data.Append($"[quote='{userName}']");
 
                         // process specidic child node
                         HtmlNode internalNodeWitHMessage = separatedNode.SelectSingleNode("/div/div/div/div[3]");
@@ -124,7 +132,7 @@ namespace ImportRunner
                         {
                             TreeWalker(internalNodeWitHMessage);
                         }
-                        _data.AppendLine("[/quote]");
+                        _data.Append("[/quote]");
                     }
                     else
                     {
@@ -132,7 +140,7 @@ namespace ImportRunner
                         string post = reg.Match(postIdRef).Value;
                         post = post.Replace("p=", string.Empty).Replace("#", string.Empty);
                         // [quote='Intrinsic' pid='528' dateline='1469909677']Dance, I think?[/quote]
-                        _data.AppendLine().Append($"[quote='{userName}' pid='{post}' dateline='1']");
+                        _data.Append($"[quote='{userName}' pid='{post}' dateline='1']");
 
                         // process specidic child node
                         HtmlNode internalNodeWitHMessage = separatedNode.SelectSingleNode("/div/div/div/div[3]");
@@ -140,13 +148,13 @@ namespace ImportRunner
                         {
                             TreeWalker(internalNodeWitHMessage);
                         }
-                        _data.AppendLine("[/quote]");
+                        _data.Append("[/quote]");
                     }
                 }
                 else
                 {
                     // [quote]It's bummer we may never see him again.  Doubt he'll get the memo on the new site.[/quote]
-                    _data.AppendLine().Append($"[quote]");
+                    _data.Append("[quote]");
 
                     // process specidic child node
                     HtmlNode internalNodeWitHMessage = separatedNode.SelectSingleNode("//div/div/div");
@@ -154,21 +162,21 @@ namespace ImportRunner
                     {
                         TreeWalker(internalNodeWitHMessage);
                     }
-                    _data.AppendLine("[/quote]");
+                    _data.Append("[/quote]");
                 }
                 return;
             }
-            if (node.Name == "video")
-            {
-                HtmlNode separatedNode = node.SelectSingleNode("//source");
-                string src = separatedNode.GetAttributeValue("src", string.Empty);
-                //Regex reg = new Regex(@".*gfycat.com/");
-                //string toRemove = reg.Match(src).Value;
-                //src = src.Replace(toRemove, string.Empty).Replace(".mp4", string.Empty).Replace(".webm", string.Empty).Replace(".ogg", string.Empty);
-                _data.Append($"[MEDIA]height=320;id={src};width=640[/MEDIA]");
-                return;
-            }
-
+            //if (node.Name == "video")
+            //{
+            //    HtmlNode separatedNode = node.SelectSingleNode("//source");
+            //    string src = separatedNode.GetAttributeValue("src", null);
+            //    //Regex reg = new Regex(@".*gfycat.com/");
+            //    //string toRemove = reg.Match(src).Value;
+            //    //src = src.Replace(toRemove, string.Empty).Replace(".mp4", string.Empty).Replace(".webm", string.Empty).Replace(".ogg", string.Empty);
+            //    _data.Append($"[MEDIA]height=320;id={src};width=640[/MEDIA]");
+            //    return;
+            //}
+            string text;
             if (!node.HasChildNodes)
             {
                 switch (node.Name)
@@ -188,26 +196,28 @@ namespace ImportRunner
                                 throw new Exception("Invalid tag " + node.Name);
                             }
                             data = data.Substring(0, data.IndexOf("?", StringComparison.OrdinalIgnoreCase));
-                            data = data.Replace("//www.youtube.com/embed/", string.Empty);
+                            int lastIndex = data.LastIndexOf("/", StringComparison.OrdinalIgnoreCase);
+                            data = data.Substring(lastIndex + 1);
 
                             string video = $"[MEDIA=youtube]{data}[/MEDIA]";
-                            _data.AppendLine(video);
+                            _data.Append(video);
                             break;
                         }
 
                         throw new Exception("Invalid tag " + node.Name);
                     case "img":
                         string src = node.GetAttributeValue("src", string.Empty);
-                        _data.AppendLine($"[img]{src}[/img]");
+                        _data.Append($"[img]{src}[/img]");
                         break;
                     case "#text":
-                        string text = node.InnerText.RemoveEndOfLineCharacter().ReplaceTabsForSingleWhiteSpace().TrimSafely();
+                        text = HttpUtility.HtmlDecode(node.InnerText.RemoveEndOfLineCharacter().ReplaceTabsForSingleWhiteSpace().TrimSafely());
                         _data.Append(text);
                         break;
                     case "div":
-                        _data.Append(node.InnerText.RemoveEndOfLineCharacter().ReplaceTabsForSingleWhiteSpace().TrimSafely());
+                        text = HttpUtility.HtmlDecode(node.InnerText.RemoveEndOfLineCharacter().ReplaceTabsForSingleWhiteSpace().TrimSafely());
+                        _data.Append(text);
                         break;
-                    
+
                     default:
                         throw new Exception("Invalid tag " + node.Name);
                 }
--- TestsCases/WhenContentParsing.cs
+++ TestsCases/WhenContentParsing.cs
@@ -10,67 +10,193 @@ namespace TestsCases
         [TestMethod]
         public void ThenATagsParseProperly()
         {
-            ContentConverter content = new ContentConverter();
-            content.Html = @"<blockquote class=""postcontent restore "">
-							NASA will send second Mars rover in 2020, send humans in 2030s - <a href=""http://arstechnica.com/science/2012/12/nasa-will-send-second-mars-rover-in-2020-send-humans-in-2030s/"" target=""_blank"">http://arstechnica.com/science/2012/...mans-in-2030s/</a>
-						</blockquote>";
+            ContentConverter content = new ContentConverter
+            {
+                Html =
+                    @"<blockquote class=""postcontent restore "">a<a href=""http://arstechnica.com/science/2012/12/nasa-will-send-second-mars-rover-in-2020-send-humans-in-2030s/"" target=""_blank"">link/</a>b</blockquote>"
+            };
             var outString = content.ConvertContent();
+            Assert.AreEqual("a[url=http://arstechnica.com/science/2012/12/nasa-will-send-second-mars-rover-in-2020-send-humans-in-2030s/]link/[/url]b", outString);
         }
 
         [TestMethod]
         public void ThenBrTagsParseProperly()
         {
+            ContentConverter content = new ContentConverter
+            {
+                Html = @"<blockquote class=""postcontent restore "">a<br>b</blockquote>"
+            };
+            var outString = content.ConvertContent();
+            Assert.AreEqual($"a{Environment.NewLine}b", outString);
         }
 
         [TestMethod]
         public void ThenImgTagsParseProperly()
-        {
+        {//
+            ContentConverter content = new ContentConverter
+            {
+                Html =
+                    @"<blockquote class=""postcontent restore "">a<img src=""http://i.imgur.com/qDLR3.jpg"" border=""0"" alt="""">b</blockquote>"
+            };
+            var outString = content.ConvertContent();
+            Assert.AreEqual("a[img]http://i.imgur.com/qDLR3.jpg[/img]b", outString);
         }
 
         [TestMethod]
         public void ThenYouTubeTagsParseProperly()
         {
+            ContentConverter content = new ContentConverter
+            {
+                Html = @"<blockquote class=""postcontent restore "">
+							
+<iframe class=""restrain"" title=""YouTube video player"" width=""640"" height=""390"" src=""http://www.youtube.com/embed/B5sks1-EGfw?wmode=opaque"" frameborder=""0""></iframe>
+
+						</blockquote>"
+            };
+            var outString = content.ConvertContent();
+            Assert.AreEqual("[MEDIA=youtube]B5sks1-EGfw[/MEDIA]", outString);
+
+            content = new ContentConverter
+            {
+                Html = @"<blockquote class=""postcontent restore ""><iframe class=""restrain"" title=""YouTube video player"" width=""640"" height=""390"" src=""//www.youtube.com/embed/W2ET8ghJaBE?wmode=opaque"" frameborder=""0""></iframe><iframe class=""restrain"" title=""YouTube video player"" width=""640"" height=""390"" src=""//www.youtube.com/embed/ojpQ3LCeQTY?wmode=opaque"" frameborder=""0""></iframe></blockquote>"
+            };
+            outString = content.ConvertContent();
+            Assert.AreEqual("[MEDIA=youtube]W2ET8ghJaBE[/MEDIA][MEDIA=youtube]ojpQ3LCeQTY[/MEDIA]", outString);
+
+
         }
 
         [TestMethod]
         public void ThenBTagsParseProperly()
-        {
+        {//
+            ContentConverter content = new ContentConverter
+            {
+                Html = @"<blockquote class=""postcontent restore "">a<b>Brutal</b>b</blockquote>"
+            };
+            var outString = content.ConvertContent();
+            Assert.AreEqual("a[b]Brutal[/b]b", outString);
         }
+
 
         [TestMethod]
         public void ThenUTagsParseProperly()
         {
+            ContentConverter content = new ContentConverter
+            {
+                Html = @"<blockquote class=""postcontent restore "">a<u>Brutal</u>b</blockquote>"
+            };
+            var outString = content.ConvertContent();
+            Assert.AreEqual("a[u]Brutal[/u]b", outString);
         }
 
         [TestMethod]
         public void ThenITagsParseProperly()
         {
+            ContentConverter content = new ContentConverter
+            {
+                Html = @"<blockquote class=""postcontent restore "">a<i>Brutal</i>b</blockquote>"
+            };
+            var outString = content.ConvertContent();
+            Assert.AreEqual("a[i]Brutal[/i]b", outString);
         }
 
         [TestMethod]
         public void ThenQuoteWithoutAnAuthorTagsParseProperly()
         {
+            ContentConverter content = new ContentConverter
+            {
+                Html = @"<blockquote class=""postcontent restore "">a
+							<div class=""bbcode_container"">
+	<div class=""bbcode_quote"">
+		<div class=""quote_container"">
+			<div class=""bbcode_quote_container""></div>
+			
+				Let's lift a glass of Kanar to the best ST character of all time.
+			
+		</div>
+	</div>
+</div>I beg to differ.</blockquote>"
+            };
+            var outString = content.ConvertContent();
+            Assert.AreEqual("a[quote]Let's lift a glass of Kanar to the best ST character of all time.[/quote]I beg to differ.", outString);
+
         }
 
-        [TestMethod]
-        public void ThenQuoteWithAnAuthorTagsParseProperly()
-        {
-        }
+
         [TestMethod]
         public void ThenQuoteWithAuthorTagsParseProperly()
         {
+            ContentConverter content = new ContentConverter
+            {
+                Html = @"<blockquote class=""postcontent restore "">a
+							<div class=""bbcode_container"">
+	<div class=""bbcode_quote"">
+		<div class=""quote_container"">
+			<div class=""bbcode_quote_container""></div>
+			
+				<div class=""bbcode_postedby"">
+					<img src=""http://www.rerolled.org/images/misc/quote_icon.png"" alt=""Quote""> Originally Posted by <strong>Flight</strong>
+					<a href=""http://www.rerolled.org/showthread.php?121-Timeline-of-what-happened-on-this-epic-day/page2&amp;p=1149#post1149#post1149"" rel=""nofollow""><img class=""inlineimg"" src=""http://www.rerolled.org/images/buttons/viewpost-right.png"" alt=""View Post""></a>
+				</div>
+				<div class=""message"">Eomer got Duppin'ed.<br>
+<br>
+/ClayDavis</div>
+			
+		</div>
+	</div>
+</div>haha, hardly, I even said &quot;maybe I'm pulling a Duppin&quot; in the post. I guess I just couldn't wrap my mind around someone actually inventing a fucking RAPE PREGNANCY story of all fucking things. It's not like I spent weeks white knighting.<br>
+<br>
+Still. I feel shame.b
+						</blockquote>" };
+            var outString = content.ConvertContent();
+            Assert.AreEqual("a[quote=\'Flight\']Eomer got Duppin\'ed.\r\n\r\n/ClayDavis[/quote]haha, hardly, I even said \"maybe I\'m pulling a Duppin\" in the post. I guess I just couldn\'t wrap my mind around someone actually inventing a fucking RAPE PREGNANCY story of all fucking things. It\'s not like I spent weeks white knighting.\r\n\r\nStill. I feel shame.b", outString);
         }
 
         [TestMethod]
         public void ThenSpolierTagsParseProperly()
         {
+            ContentConverter content = new ContentConverter
+            {
+                Html =
+                    @"<blockquote class=""postcontent restore "">a<div style=""margin: 5px 20px 20px;""> <div class=""smallfont"" style=""margin-bottom: 2px;""><b>Spoiler:</b>&nbsp;  <input value=""Show"" style=""margin: 0px; padding: 0px; font-size: 10px;"" onclick=""if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') { this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = '';this.innerText = ''; this.value = 'Hide'; } else { this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none'; this.innerText = ''; this.value = 'Show'; }"" type=""button""> </div> <div class=""alt2"" style=""border: 1px inset ; margin: 0px; padding: 6px;""> <div style=""display: none;"">Snape kills dumberdole</div> </div> </div>b</blockquote>"
+            };
+
+            var outString = content.ConvertContent();
+            Assert.AreEqual("a[spoiler]Snape kills dumberdole[/spoiler]b", outString);
         }
 
         [TestMethod]
         public void ThenTextTagsParseProperly()
         {
+            ContentConverter content = new ContentConverter
+            {
+                Html = @"<blockquote class=""postcontent restore "">a le dasd</blockquote>"
+            };
+            var outString = content.ConvertContent();
+            Assert.AreEqual("a le dasd", outString);
         }
 
-         
+
+        [TestMethod]
+        public void ThenTextSpansParseProperly()
+        {
+            ContentConverter content = new ContentConverter
+            {
+                Html =
+                    @"<blockquote class=""postcontent restore ""><span style=""font-family: Arial""><span style=""font-family: Book Antiqua"">&quot;Bishop owns 20 acres in Douglass, a town about 160 miles north of Houston. He used to raise poultry and goats on the land where he lives with his wife and 16-year-old daughter, he said, but sold the animals about two years ago because of the planned pipeline. Initially, the Vietnam War veteran said, he fought the company's attempt to condemn his land, but settled because he could not afford the lawyer's fees of $10,000.<br>
+Bishop said he settled under &quot;duress,&quot; so he bought a law book and decided to defend himself. Since then, he has filed a lawsuit in Austin against the Texas Railroad Commission, the state agency that oversees pipelines, arguing it failed to properly investigate the pipeline and protect groundwater, public health and safety.<br>
+Aware that the oil giant could have a battery of lawyers and experts at the hearing later this month, Bishop, a 64-year-old retired chemist currently in medical school, said he is determined to fight.<br>
+&quot;Bring 'em on. I'm a United States Marine. I'm not afraid of anyone. I'm not afraid of them,&quot; he said. &quot;When I'm done with them, they will know that they've been in a fight. I may not win, but I'm going to hurt them.&quot;<br>
+</span></span></blockquote>"
+            };
+            var outString = content.ConvertContent();
+            Assert.AreEqual(@"""Bishop owns 20 acres in Douglass, a town about 160 miles north of Houston. He used to raise poultry and goats on the land where he lives with his wife and 16-year-old daughter, he said, but sold the animals about two years ago because of the planned pipeline. Initially, the Vietnam War veteran said, he fought the company's attempt to condemn his land, but settled because he could not afford the lawyer's fees of $10,000.
+Bishop said he settled under ""duress,"" so he bought a law book and decided to defend himself. Since then, he has filed a lawsuit in Austin against the Texas Railroad Commission, the state agency that oversees pipelines, arguing it failed to properly investigate the pipeline and protect groundwater, public health and safety.
+Aware that the oil giant could have a battery of lawyers and experts at the hearing later this month, Bishop, a 64-year-old retired chemist currently in medical school, said he is determined to fight.
+""Bring 'em on. I'm a United States Marine. I'm not afraid of anyone. I'm not afraid of them,"" he said. ""When I'm done with them, they will know that they've been in a fight. I may not win, but I'm going to hurt them.""
+", outString);
+        }
+
+
     }
 }
